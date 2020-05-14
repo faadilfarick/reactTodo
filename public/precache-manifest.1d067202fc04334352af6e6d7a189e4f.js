97eab1d04d3275a7dba21d34d7b1ac488aaff06a
Faadil <faadil.farick@outlook.com>
2020-05-14 16:20:22 +0530

    rearranged

---
+++ public/precache-manifest.1d067202fc04334352af6e6d7a189e4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a47f6b6b5525a83c7885abed83d6ba0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3514007e735c3354e22c",
+    "url": "/static/css/main.c0e84278.chunk.css"
+  },
+  {
+    "revision": "109af8a4d95508d20b6e",
+    "url": "/static/js/2.f6c98408.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f6c98408.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3514007e735c3354e22c",
+    "url": "/static/js/main.7ecb9082.chunk.js"
+  },
+  {
+    "revision": "310c168f5b4a8bb90b3d",
+    "url": "/static/js/runtime-main.eefa256f.js"
+  }
+]);
